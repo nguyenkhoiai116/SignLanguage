f773ab548bb61b0d2cd3de44496bd4391d98b776
nguyenkhoiai116 <nguyenkhoi.ai116@gmail.com>
2025-12-20 11:11:11 +0700

    them file data

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-print("Hi_")
-print("There")
+# tạo folder từ tên là a -> z vd data/a \\ data/b ...
+import os
+for char in range(ord('a'), ord('z') + 1):
+    folder_name = f"data/{chr(char)}"
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+        print(f"Folder '{folder_name}' created.")
+    else:
+        print(f"Folder '{folder_name}' already exists.")
